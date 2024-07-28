4d2ac4e7a2bd927abd6fbcb9190084c1fd4343f2
Sabiq Hashil <sabiqhashilkp786@gmail.com>
2024-07-28 17:25:26 +0530

    "Updated ProjectsMain component: changed AOS animation direction for h1 element and added AOS animation to project cards"

--- src/Components/ProjectsMain.jsx
+++ src/Components/ProjectsMain.jsx
@@ -14,14 +14,18 @@ const Projects = () => {
       className="p-20 lg:mb-20 flex flex-col items-center justify-center"
     >
       <h1
-        data-aos="fade-right"
+        data-aos="fade-left"
         className="text-[52px] font-semibold mb-20 leading-normal uppercase text-fuchsia-500"
       >
         Projects
       </h1>
       <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 justify-around gap-10">
         {ProjectsData.map((project) => (
-          <div key={project.id} className="card cursor-pointer">
+          <div
+            data-aos="fade-up"
+            key={project.id}
+            className="card cursor-pointer"
+          >
             <Link to={`/projects/${project.id}`}>
               <img src={project.image} alt="project logo" />
               <div className="card__content">
