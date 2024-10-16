dd1a3e04db9a9bfb95916b7227ef0cd6b4c41f9e
Sabiq Hashil <99120128+SabiqHashil@users.noreply.github.com>
2024-10-17 01:02:00 +0530

    "Updated WhatsAppButton component with new phone number placeholder and added animation to button style; added bounce animation to index.css"

--- src/Components/Reusable/WhatsAppButton.jsx
+++ src/Components/Reusable/WhatsAppButton.jsx
@@ -2,7 +2,7 @@ import { FaWhatsapp } from "react-icons/fa";
 
 const WhatsAppButton = () => {
   const redirectToWhatsApp = () => {
-    const phoneNumber = "+917559873623";
+    const phoneNumber = "+91"; // Add the actual number here
     const message = "Hello, I have a query.";
     const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(
       message
@@ -13,7 +13,10 @@ const WhatsAppButton = () => {
   return (
     <button
       onClick={redirectToWhatsApp}
-      className="bg-[#25D366] text-white lg:p-4 p-3 rounded-full hover:bg-green-800 fixed border-2 border-fuchsia-500 img_glow lg:bottom-10 bottom-8 lg:right-10 right-8 z-50"
+      className="bg-[#25D366] text-white lg:p-4 p-3 rounded-full hover:bg-green-800 fixed border-2 border-fuchsia-500 img_glow lg:bottom-10 bottom-8 lg:right-10 right-8 z-50 transition duration-300 ease-in-out"
+      style={{
+        animation: "bounce 1.5s infinite",
+      }}
     >
       <FaWhatsapp size={28} />
     </button>
