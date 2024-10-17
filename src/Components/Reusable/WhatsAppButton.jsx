c64f51abdc169b956580a00ffef2c417f2e73de5
Sabiq Hashil <99120128+SabiqHashil@users.noreply.github.com>
2024-10-17 09:25:28 +0530

    "Updated WhatsAppButton component to use 'pulseGlow' animation instead of 'bounce', and modified the animation keyframes in index.css"

--- src/Components/Reusable/WhatsAppButton.jsx
+++ src/Components/Reusable/WhatsAppButton.jsx
@@ -2,7 +2,7 @@ import { FaWhatsapp } from "react-icons/fa";
 
 const WhatsAppButton = () => {
   const redirectToWhatsApp = () => {
-    const phoneNumber = "+917559873623"; // Add the actual number here
+    const phoneNumber = "+917559873623";
     const message = "Hello, I have a query.";
     const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(
       message
@@ -15,7 +15,7 @@ const WhatsAppButton = () => {
       onClick={redirectToWhatsApp}
       className="bg-[#25D366] text-white lg:p-4 p-3 rounded-full hover:bg-green-800 fixed border-2 border-fuchsia-500 img_glow lg:bottom-10 bottom-8 lg:right-10 right-8 z-50 transition duration-300 ease-in-out"
       style={{
-        animation: "bounce 1.5s infinite",
+        animation: "pulseGlow 2s infinite",
       }}
     >
       <FaWhatsapp size={28} />
